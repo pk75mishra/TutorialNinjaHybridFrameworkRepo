f53b7c06e69d0348f9178da27f9c2eefa3380b87
pc <pc@DESKTOP-3RCQPKA>
2024-09-02 16:12:10 +0530

    Added more details on comment.

--- src/test/java/com/tutorialninja/qa/testcases/SearchTest.java
+++ src/test/java/com/tutorialninja/qa/testcases/SearchTest.java
@@ -16,7 +16,7 @@ import com.tutorialninja.qa.base.Base;
 import com.tutorialninja.qa.pages.HomePage;
 import com.tutorialninja.qa.pages.SearchPage;
 
-//Updated comment
+//Updated comment- Added more details
 
 public class SearchTest extends Base {
 
